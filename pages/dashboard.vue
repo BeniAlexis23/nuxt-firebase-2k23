3ab88f680303821fe4dc8c27c10ea009c118243d
Balh23 <beni_luyo2023@outlook.com>
2023-11-15 08:13:27 -0500

    first commit

---
+++ pages/dashboard.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const { logout } = useFirebaseAuth();
+const toast = useToast();
+
+async function handleLogout() {
+  try {
+    await logout();
+    toast.add({
+      title: "Adios...",
+      timeout: 500,
+      callback: async () => {
+        await navigateTo("/login");
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1>ADMIN (Ruta protegida)</h1>
+    <UButton class="mt-5" @click="handleLogout">Logout</UButton>
+  </div>
+</template>
